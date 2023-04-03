54b201b195caf11f98a6f81a8bafcb972327168f
bub3690 <bub3690@naver.com>
2023-04-04 03:25:15 +0900

    FEAT: Update background remover

--- service/txt2img_service.py
+++ service/txt2img_service.py
@@ -2,7 +2,7 @@ import bentoml
 from bentoml.io import Image, JSON, Multipart
 from starlette.middleware.cors import CORSMiddleware
 import torch
-#from rembg import remove
+from rembg import remove
 
 from contextlib import ExitStack
 
@@ -25,6 +25,7 @@ class StableDiffusionRunnable(bentoml.Runnable):
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
 
         self.txt2img_pipe = bentoml.diffusers.load_model(TXT2IMG_MODEL_TAG,enable_xformers=False)#.to(self.device)
+        #pipeline을 매번 만드느라 느린 단점이 있다.
 
     @bentoml.Runnable.method(batchable=False, batch_dim=0)
     def txt2img(self, parsed_json):
@@ -33,16 +34,18 @@ class StableDiffusionRunnable(bentoml.Runnable):
 
         image = self.txt2img_pipe(src_prompt,
         guidance_scale=7.5,
-        negative_prompt=",".join([''])
+        negative_prompt=",".join(['']),
         ).images[0]
+        image = remove(image)#png 출력
+        
         return image
 
 stable_diffusion_runner = bentoml.Runner(StableDiffusionRunnable, name='stable_diffusion_runner', max_batch_size=10)
 
-svc = bentoml.Service("stable_diffusion_fp32", runners=[stable_diffusion_runner])
+svc = bentoml.Service("stable_diffusion_fp16", runners=[stable_diffusion_runner])
 svc.add_asgi_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"], expose_headers=["*"])
 
-@svc.api(input=JSON(),output=Image)
+@svc.api(input=JSON(),output=Image(mime_type="image/png"))
 def txt2img(parsed_json):
     #print(parsed_json)
     image = stable_diffusion_runner.txt2img.run(parsed_json)
