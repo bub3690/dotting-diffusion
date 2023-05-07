18ea784aa613ba18c671487b00c524248f3fe6a1
bub3690 <bub3690@naver.com>
2023-05-08 01:28:43 +0900

    FEAT : Update multi image diffusion

--- bentoml/runners/diffusion_runner.py
+++ bentoml/runners/diffusion_runner.py
@@ -9,6 +9,7 @@ class StableDiffusionRunnable(bentoml.Runnable):
     SUPPORTS_CPU_MULTI_THREADING = True
 
     def __init__(self):
+        # 이부분을 bentoml model로 교체해야함.
         TXT2IMG_MODEL_TAG = "PublicPrompts/All-In-One-Pixel-Model"
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         if self.device == "cuda":
@@ -30,3 +31,25 @@ class StableDiffusionRunnable(bentoml.Runnable):
         image = remove(image[0])
 
         return image
+    
+    
+    @bentoml.Runnable.method(batchable=False, batch_dim=0)
+    def txt2img_multi(self, parsed_json)->list:
+        print("multi")
+        #여러 이미지를 출력하는 api
+        src_prompt = parsed_json.get("prompt") + ",pixelsprite,full body game asset,background chroma plain green"
+
+        num_imgs = parsed_json.get("num_imgs") # 서버 성능에 따라 최대 이미지 수는 달라질 것.
+        if num_imgs is None:
+            num_imgs = 2
+
+        image = self.txt2img_pipe(src_prompt,
+        guidance_scale=7.5,
+        negative_prompt=",".join(['']),
+        num_images_per_prompt=num_imgs,
+        ).images
+        
+        images = [ remove(sample) for sample in image]#png 출력
+        #image = remove(image[0])
+
+        return images
--- bentoml/stablediffsuion_service.py
+++ bentoml/stablediffsuion_service.py
@@ -5,7 +5,60 @@ from runners.diffusion_runner import StableDiffusionRunnable
 from runners.image_remover_runner import ImageRemoverRunnable
 
 from io import BytesIO
+from google.cloud import storage
 
+
+import random
+import string
+import time
+
+# 현재 시간을 기반으로 파일 이름을 생성합니다.
+# 파일 이름은 현재 시간(초단위)을 기준으로 생성됩니다.
+def generate_filename():
+    # 현재 시간(초)을 가져옵니다.
+    current_time = int(time.time())
+
+    # 무작위로 구성된 알파벳과 숫자로 구성된 문자열을 생성합니다.
+    # 문자열의 길이는 8자리로 제한합니다.
+    random_chars = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
+
+    # 현재 시간(초)과 무작위 문자열을 조합하여 파일 이름을 생성합니다.
+    filename = f"{current_time}_{random_chars}"
+    
+    return filename
+
+
+
+
+
+def upload_blob(bucket_name, image_file):
+    """Uploads a file to the bucket."""
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+    # The path to your file to upload
+    # source_file_name = "local/path/to/file"
+    # The ID of your GCS object
+    # destination_blob_name = "storage-object-name"
+
+    image_file_name = generate_filename()#난수로 생성시키기
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(image_file_name)
+    
+    with BytesIO() as output:
+        image_file.save(output, format='PNG')
+        contents = output.getvalue()
+        
+    blob.upload_from_string(contents, content_type='image/png')
+    # Make the file public
+    blob.make_public()
+
+    # Print the URL of the file
+    print(
+        f"File uploaded to {blob.public_url}."
+    )
+    return blob.public_url
 
 
 
@@ -38,6 +91,27 @@ def txt2img(parsed_json):
 
     #print(files.keys())
     return image
+
+
+@stable_diffusion_fp16.api(input=JSON(),output=JSON())
+def txt2img_multi(parsed_json):
+    #output : image path
+    files = JSON()
+    
+    print(parsed_json)
+    image_list = stable_diffusion_runner.txt2img_multi.run(parsed_json) #List 형태
+    
+    # upload image to gcp storage
+    image_list = [ upload_blob("genai-bucket",image) for image in image_list ]#리스트 하나씩 인풋
+    
+    
+    for i in range(len(image_list)):
+        file_name = f'Image{i+1}'
+        files[file_name] = image_list[i]
+
+    print(files)
+    return files
+
 
 
 
